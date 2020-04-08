e2f1668a1fca57a4a21e70b953928e962ce76982
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-08 17:16:53 -0600

    Update to answer spec and model to include comments
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Steve Anderson <alerrian@users.noreply.github.com>
    Co-Authored-By: Jordan Williams <jordan.wallace.williams@gmail.com>

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,8 @@ class Answer < ApplicationRecord
   validates_presence_of :content
   validates_presence_of :upvotes
 
+  has_many :comments, as: :commentable
+
   belongs_to :user
-  belongs_to :question 
+  belongs_to :question
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe Answer, type: :model do
     it {should validate_presence_of :content}
     it {should validate_presence_of :upvotes}
   end
-  
+
   describe 'relationships' do
     it {should belong_to :user}
     it {should belong_to :question}
+    it {should have_many :comments}
   end
 end

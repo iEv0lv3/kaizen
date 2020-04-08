5e3a8507b7882ae17b38c578ac92b7efdd8a003a
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-08 15:59:20 -0600

    Create user model to validate presences and relationships
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Steve Anderson <alerrian@users.noreply.github.com>
    Co-Authored-By: Jordan Williams <jordan.wallace.williams@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
+  validates_uniqueness_of :email
+  validates_presence_of :encrypted_password
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :cohort
+  validates_presence_of :status
+
+  has_many :questions 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable

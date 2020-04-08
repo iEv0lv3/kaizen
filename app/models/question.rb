3d61f20cc73a61b981bafdb59ee86a9f9e4b78ca
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-08 17:16:17 -0600

    Update to question spec and model to include comments
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Steve Anderson <alerrian@users.noreply.github.com>
    Co-Authored-By: Jordan Williams <jordan.wallace.williams@gmail.com>

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,5 +4,8 @@ class Question < ApplicationRecord
   validates_presence_of :upvotes
   validates_presence_of :type
 
-  belongs_to :user 
+  has_many :comments, as: :commentable
+  has_many :answers
+
+  belongs_to :user
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Question, type: :model do
     it {should validate_presence_of :content}
     it {should validate_presence_of :upvotes}
     it {should validate_presence_of :type}
-  end 
+  end
   describe 'relationships' do
     it {should belong_to :user}
+    it {should have_many :comments}
+    it {should have_many :answers}
   end
 end

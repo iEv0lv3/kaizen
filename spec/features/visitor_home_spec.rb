fa07c97a58edf50d6dc1b88afbb20763f5d71de0
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-08 17:49:19 -0600

    Add test to check home button

--- spec/features/visitor_home_spec.rb
+++ spec/features/visitor_home_spec.rb
@@ -12,5 +12,15 @@ RSpec.describe 'As a visitor' do
         expect(page).to have_content('Ask a Question')
       end
     end
+
+    it 'can click on the home link and be taken back to the root page' do
+      visit root_path
+
+      within '.nav' do
+        click_on 'Turing :: SO'
+
+        expect(current_path).to eq(root_path)
+      end
+    end
   end
 end

1638692865507456c811beb093ef8f68ee49e8e5
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-08 16:00:57 -0600

    Update schema with questions table
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Steve Anderson <alerrian@users.noreply.github.com>
    Co-Authored-By: Jordan Williams <jordan.wallace.williams@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_192727) do
+ActiveRecord::Schema.define(version: 2020_04_08_211835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "subject"
+    t.text "content"
+    t.integer "upvotes"
+    t.integer "awards"
+    t.integer "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_04_08_192727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end

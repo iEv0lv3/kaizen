4bf7ba8fd4898a0023c432c3d47b43b19491cd3f
sasloan <sebastiansloan94@gmail.com>
2020-04-09 14:41:45 -0600

    spec to register a new user complete

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -12,16 +12,18 @@ RSpec.describe 'As a Visitor' do
 
             expect(current_path).to eq("/users/sign_up")
 
+            choose :user_status_1
+            fill_in :user_first_name, with: "Fakey"
+            fill_in :user_last_name, with: "McFakerson"
+            fill_in :user_cohort, with: "1911"
             fill_in :user_email, with: "faker@fake.com"
             fill_in :user_password, with: "Fakeymcfackerson"
             fill_in :user_password_confirmation, with: "Fakeymcfackerson"
 
             click_on "Sign up"
 
-            expect(current_path).to eq("/users")
-
-            # user = User.last
-            # require 'pry'; binding.pry
+            expect(current_path).to eq("/")
+            expect(User.last.first_name).to eq("Fakey")
         end
     end
 end
--- spec/features/users/profile/show_spec.rb
+++ spec/features/users/profile/show_spec.rb
@@ -1,26 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'As a User' do 
-    describe 'When I visit my profile' do 
-        before :each do 
-            @user = create(:user)
-            @user.confirm
-
-            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-        end
-
+    describe 'When I visit my profile' do
         it 'By first logging in on the welcome page' do 
+            user = create(:user)
+            user.confirm
 
             visit '/'
 
-            expect(page).to have_content("Login")
+            expect(page).to have_content("Sign In")
 
-            click_on "Login"
+            click_on "Sign In"
 
             expect(current_path).to eq('/users/sign_in')
 
-            fill_in :user_email, with: @user.email
-            fill_in :user_password, with: @user.password
+            fill_in :user_email, with: user.email
+            fill_in :user_password, with: user.password
+            
 
             click_on "Log in"
 

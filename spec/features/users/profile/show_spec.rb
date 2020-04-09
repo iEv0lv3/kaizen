f5043ced3b20d65302bb04e999282cbbd42111bf
sasloan <sebastiansloan94@gmail.com>
2020-04-09 17:58:26 -0600

    merge from master

--- spec/features/users/profile/show_spec.rb
+++ spec/features/users/profile/show_spec.rb
@@ -23,4 +23,34 @@ RSpec.describe 'As a User' do
             expect(current_path).to eq("/")
         end
     end
+
+    describe 'When I am on my profile page' do 
+        before :each do 
+            user = create(:user)
+            user.confirm
+
+            visit '/'
+
+            expect(page).to have_content("Sign In")
+
+            click_on "Sign In"
+
+            expect(current_path).to eq('/users/sign_in')
+
+            fill_in :user_email, with: user.email
+            fill_in :user_password, with: user.password
+            
+
+            click_on "Log in"
+
+            expect(current_path).to eq("/")
+
+            click_on "Profile"
+
+            expect(current_path).to eq(profile_path)
+        end
+
+        it 'I can see all of my information' do 
+
+            
 end

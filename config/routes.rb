e32e2b1aa9a06e828e94264106f2266143f03d50
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-09 17:45:18 -0600

    Add route for a new question to a users question controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get 'profile', to: 'users/profile#show'
+    get 'questions/new', to: 'users/questions#new'
   end
 
   root to: 'welcome#index'

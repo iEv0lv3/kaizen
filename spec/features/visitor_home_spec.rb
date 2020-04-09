6421eb12d85d8655c63ff6d11ed399412c3fd6e3
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-08 22:48:10 -0600

    Add test for visitor home things

--- spec/features/visitor_home_spec.rb
+++ spec/features/visitor_home_spec.rb
@@ -18,9 +18,30 @@ RSpec.describe 'As a visitor' do
 
       within '.nav' do
         click_on 'Turing :: SO'
-
-        expect(current_path).to eq(root_path)
       end
+
+      expect(current_path).to eq(root_path)
+    end
+
+    it 'can register for an account' do
+      visit root_path
+
+      within '.nav' do
+        click_on 'Register'
+      end
+
+      expect(current_path).to eq(new_user_registration_path)
+
+      within '.new_user' do
+        fill_in 'Email', with: 'email@example.com'
+        fill_in 'Password', with: 'newpass'
+        fill_in 'Password confirmation', with: 'newpass'
+
+        click_on 'Sign up'
+      end
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
     end
   end
 end

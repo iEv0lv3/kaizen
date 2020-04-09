5fd269ed1162b7aa1695bf079b02f0552f4350e9
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-09 10:35:48 -0600

    Fix spec folder for rubocop style changes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'validations' do
-    it {should validate_presence_of :content}
-    it {should validate_presence_of :upvotes}
+    it { should validate_presence_of :content }
+    it { should validate_presence_of :upvotes }
   end
 
   describe 'relationships' do
-    it {should belong_to :user}
-    it {should belong_to :question}
-    it {should have_many :comments}
+    it { should belong_to :user }
+    it { should belong_to :question }
+    it { should have_many :comments }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it {should validate_uniqueness_of :email}
-    it {should validate_presence_of :encrypted_password}
-    it {should validate_presence_of :first_name}
-    it {should validate_presence_of :last_name}
-    it {should validate_presence_of :cohort}
-    it {should validate_presence_of :status}
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :encrypted_password }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :cohort }
+    it { should validate_presence_of :status }
   end
 
   describe 'relationships' do
-    it {should have_many :questions}
-    it {should have_many :answers}
-    it {should have_many :comments}
+    it { should have_many :questions }
+    it { should have_many :answers }
+    it { should have_many :comments }
   end
 end

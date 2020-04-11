3fe4df4d53bc80b23ee0777f3d2dea984bdb73ad
sasloan <sebastiansloan94@gmail.com>
2020-04-11 17:52:39 -0600

    Completed technical quesiton answer view

--- app/controllers/professional_forum_controller.rb
+++ app/controllers/professional_forum_controller.rb
@@ -2,8 +2,9 @@ class ProfessionalForumController < ApplicationController
   def index
     @questions = Question.professional_questions
   end
-
+  
   def show
     @question = Question.find(params[:question_id])
+    @answers = @question.answers
   end
 end
--- spec/features/answers /index_spec.rb
+++ spec/features/answers /index_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'As a Visitor' do
         before :each do 
             user = create(:user)
             user.confirm
+
+            user_2 = create(:user)
+            user_2.confirm
       
             @question_1 = Question.create!({subject: "Ruby methods",
                                             content: "What is attr_reader?",
@@ -14,13 +17,13 @@ RSpec.describe 'As a Visitor' do
             
             @answer_1 = @question_1.answers.create!({content: "The attr_reader lets other read your code.",
                                                      upvotes: 1,
-                                                     user_id: user.id
+                                                     user_id: user_2.id
 
             })
 
             @answer_2 = @question_1.answers.create!({content: "the attr_reader allows methods in the file to be read by other files.",
                                                      upvotes: 1,
-                                                     user_id: user.id
+                                                     user_id: user_2.id
 
             })
 

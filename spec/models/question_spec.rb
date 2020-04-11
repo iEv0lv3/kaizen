6b780c13e4a1843b1bc761395b938afdf5e16667
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-11 14:16:50 -0600

    Model tests for technical and professional questions methods
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,4 +12,43 @@ RSpec.describe Question, type: :model do
     it { should have_many :comments }
     it { should have_many :answers }
   end
+
+  describe 'methods' do
+    before :each do
+      user = create(:user)
+      user.confirm
+
+      @question_1 = Question.create!({subject: "Ruby methods",
+                                      content: "What is attr_reader?",
+                                      upvotes: 1,
+                                      forum: 0,
+                                      user_id: user.id})
+
+      @question_2 = Question.create!({subject: "Jobs",
+                                      content: "Why can't I get a job?",
+                                      upvotes: 1,
+                                      forum: 1,
+                                      user_id: user.id})
+
+      @question_3 = Question.create!({subject: "Ruby methods",
+                                      content: "What is attr_accessor?",
+                                      upvotes: 1,
+                                      forum: 0,
+                                      user_id: user.id})
+
+      @question_4 = Question.create!({subject: "Interviews",
+                                      content: "Best interview practices",
+                                      upvotes: 1,
+                                      forum: 1,
+                                      user_id: user.id})
+    end
+
+    it 'technical questions' do
+      expect(Question.technical_questions).to eq([@question_3, @question_1])
+    end
+
+    it 'professional questions' do
+      expect(Question.professional_questions).to eq([@question_4, @question_2])
+    end
+  end
 end

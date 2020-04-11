d416b14ccd807a7a957730862953dbdf1228ddee
Jordan <jordan.wallace.williams@gmail.com>
2020-04-11 14:16:51 -0600

    Update routes to include callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/callbacks' }
+
+  root to: 'welcome#index'
 
   devise_scope :user do
     get 'profile', to: 'users/profile#show'
@@ -9,6 +11,4 @@ Rails.application.routes.draw do
     match 'users/:id' => 'users/profile#destroy', :via => :delete, :as => :destroy_user
     get 'questions/new', to: 'users/questions#new'
   end
-
-  root to: 'welcome#index'
 end

c58352e30b28acdca448704116c9c24a83406713
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-11 14:44:00 -0600

    Show actions for professional and technical questions
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>

--- app/controllers/professional_forum_controller.rb
+++ app/controllers/professional_forum_controller.rb
@@ -2,4 +2,8 @@ class ProfessionalForumController < ApplicationController
   def index
     @questions = Question.professional_questions
   end
+
+  def show
+    @question = Question.find(params[:question_id])
+  end
 end
--- app/controllers/technical_forum_controller.rb
+++ app/controllers/technical_forum_controller.rb
@@ -2,4 +2,8 @@ class TechnicalForumController < ApplicationController
   def index
     @questions = Question.technical_questions
   end
+
+  def show
+    @question = Question.find(params[:question_id])
+  end
 end

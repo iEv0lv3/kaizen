54d729795fcc1920bc5959b11c387959b700f1d5
Jordan <jordan.wallace.williams@gmail.com>
2020-04-11 14:24:15 -0600

    Update user model to include from_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,18 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable
+
+  # devise :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.first_name = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.update(gh_token: auth['credentials']['token'].to_s)
+    end
+  end
 end

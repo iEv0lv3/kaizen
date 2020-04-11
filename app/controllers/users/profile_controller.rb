47a8e31fdbd23f4571792e8faba28f98f632b048
sasloan <sebastiansloan94@gmail.com>
2020-04-10 18:28:16 -0600

    completed delete functionality

--- app/controllers/users/profile_controller.rb
+++ app/controllers/users/profile_controller.rb
@@ -22,6 +22,15 @@ class Users::ProfileController < Users::BaseController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+        redirect_to root_url, notice: "User deleted."
+    end
+  end
+
 
   private 
 

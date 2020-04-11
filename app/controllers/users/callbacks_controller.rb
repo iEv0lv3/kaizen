c91503e98271be0899249840bb95ff91be9e8d96
Jordan <jordan.wallace.williams@gmail.com>
2020-04-11 16:09:10 -0600

    Update callbacks_controller methods to work our way

--- app/controllers/users/callbacks_controller.rb
+++ app/controllers/users/callbacks_controller.rb
@@ -1,6 +1,17 @@
 class Users::CallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    gh_auth = request.env["omniauth.auth"]
+
+    current_user.update(
+      gh_token: gh_auth['credentials']['token'],
+      gh_login: gh_auth['info']['nickname']
+    )
+
     redirect_to profile_path, notice: 'Connected to GitHub!'
+  end
+
+  def stackoverflow
+    so_auth = request.env["omniauth.auth"]
+    binding.pry
   end
 end

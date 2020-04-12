e196c1eca881843506b7cc4710c513d1280dfa73
sasloan <sebastiansloan94@gmail.com>
2020-04-12 11:15:41 -0600

    set upvote to a default of 1 in schema for answers

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_214455) do
 
   create_table "answers", force: :cascade do |t|
     t.text "content"
-    t.integer "upvotes"
+    t.integer "upvotes", default: 1
     t.integer "awards"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

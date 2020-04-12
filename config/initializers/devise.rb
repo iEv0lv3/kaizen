30a71897f282f19d90564dbc123b0a03bfbccacd
Jordan <jordan.wallace.williams@gmail.com>
2020-04-11 19:02:04 -0600

    Update devise config to properly send through StackExchange keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :github, Figaro.env.gh_client_id, Figaro.env.gh_client_secret, display: 'popup', scope: 'user:email'
-  config.omniauth :stackexchange, Figaro.env.so_client_id, Figaro.env.so_client_secret, display: 'popup', callback_url: '/users/auth/stackexchange/callback'
+  config.omniauth :stackexchange, client_id: Figaro.env.so_client_id, client_secret: Figaro.env.so_client_secret, public_key: Figaro.env.so_key, display: 'popup'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

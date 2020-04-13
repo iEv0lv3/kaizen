633994b184044a7f46c1f5f457a5d3472b3309b5
sasloan <sebastiansloan94@gmail.com>
2020-04-13 15:23:15 -0600

    refactor test branch

--- spec/features/questions/answers/index_spec.rb
+++ spec/features/questions/answers/index_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'As a Visitor' do
             user = create(:user)
             user.confirm
 
-            user_2 = create(:user)
-            user_2.confirm
+            @user_2 = create(:user)
+            @user_2.confirm
 
             @question_1 = Question.create!({subject: "Ruby methods",
                                             content: "What is attr_reader?",
@@ -23,25 +23,25 @@ RSpec.describe 'As a Visitor' do
 
             @answer_1 = @question_1.answers.create!({content: "The attr_reader lets other read your code.",
                                                      upvotes: 1,
-                                                     user_id: user_2.id
+                                                     user_id: @user_2.id
 
             })
 
             @answer_2 = @question_1.answers.create!({content: "the attr_reader allows methods in the file to be read by other files.",
                                                      upvotes: 1,
-                                                     user_id: user_2.id
+                                                     user_id: @user_2.id
 
             })
 
             @answer_3 = @question_2.answers.create!({content: "Because you are not trying. ",
                                                      upvotes: 1,
-                                                     user_id: user_2.id
+                                                     user_id: @user_2.id
 
             })
 
             @answer_4 = @question_2.answers.create!({content: "Jobs are hard to come by, remember turing has a staff that you can reach out to to help you get there. hang in there.",
                                                      upvotes: 1,
-                                                     user_id: user_2.id
+                                                     user_id: @user_2.id
 
             })
 
@@ -68,6 +68,68 @@ RSpec.describe 'As a Visitor' do
            expect(page).not_to have_content(@answer_1.content)
            expect(page).not_to have_content(@answer_2.content)
 
+        end
+
+        it 'and if the answer is one that I made then I can delete it' do
+            visit '/'
+        
+            expect(current_path).to eq('/')
+        
+            expect(page).to have_content('Sign In')
+        
+            click_on 'Sign In'
+        
+            expect(current_path).to eq('/users/sign_in')
+        
+            fill_in :user_email, with: @user_2.email
+            fill_in :user_password, with: @user_2.password
+        
+            click_on 'Log in'
+        
+            expect(current_path).to eq('/')
+        
+            visit "/questions/#{@question_1.id}"
+        
+            expect(current_path).to eq("/questions/#{@question_1.id}")
+        
+            within "#answer-#{@answer_1.id}" do
+                expect(page).to have_link('Delete')
+                click_on 'Delete'
+            end
+        
+            expect(current_path).to eq("/questions/#{@question_1.id}")
+        
+            expect(page).not_to have_content(@answer_1.content)
+        end
+    
+        it 'and if the answer is not one that I made then I can not delete it' do
+            user_3 = create(:user)
+            user_3.confirm
+
+            visit '/'
+
+            expect(current_path).to eq('/')
+
+            expect(page).to have_content('Sign In')
+
+            click_on 'Sign In'
+
+            expect(current_path).to eq('/users/sign_in')
+
+            fill_in :user_email, with: user_3.email
+            fill_in :user_password, with: user_3.password
+
+            click_on 'Log in'
+
+            expect(current_path).to eq('/')
+
+            visit "/questions/#{@question_1.id}"
+
+            expect(current_path).to eq("/questions/#{@question_1.id}")
+
+            within "#answer-#{@answer_1.id}" do
+                expect(page).not_to have_link('Delete')
+            end
         end
     end
 end

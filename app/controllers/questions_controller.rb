571bd8e0d473780ebede0c0e81e037d890276e1b
sasloan <sebastiansloan94@gmail.com>
2020-04-13 15:16:49 -0600

    Created index functionality for questions comments

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,5 +2,6 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
     @answers = @question.answers
+    @comments = @question.comments
   end
 end

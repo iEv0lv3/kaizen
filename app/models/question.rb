a5d1f027e676fffc80e32a0d19a9a5b23177568e
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-12 21:06:19 -0600

    Update enum

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 class Question < ApplicationRecord
   validates_presence_of :subject
   validates_presence_of :content
-  validates_presence_of :upvotes
   validates_presence_of :forum
 
-  enum forum: ["technical", "professional"]
+  enum forum: %i[
+    technical
+    professional
+  ]
 
   has_many :comments, as: :commentable
   has_many :answers

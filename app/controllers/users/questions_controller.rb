dc14efa24909903a254c4975bc55930a7c05b74b
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-12 21:07:28 -0600

    Create create action in questions controller

--- app/controllers/users/questions_controller.rb
+++ app/controllers/users/questions_controller.rb
@@ -4,6 +4,29 @@ class Users::QuestionsController < Users::BaseController
   end
 
   def create
-    require "pry"; binding.pry
+    forum = params[:question][:forum].to_i
+    params[:question][:forum] = forum
+
+    question = Question.new(question_params)
+    question.user_id = current_user.id
+    if question.save
+      if question.forum == "technical"
+        redirect_to '/technical_forum'
+        flash[:success] = "Your question was successfully submitted!"
+      else
+        redirect_to '/professional_forum'
+        flash[:success] = "Your question was successfully submitted!"
+      end
+    else
+      redirect_to '/questions/new'
+      flash[:error] = question.errors.full_messages.to_sentence
+    end
   end
+
+
+  private
+
+    def question_params
+      params.require(:question).permit(:subject, :content, :forum)
+    end
 end

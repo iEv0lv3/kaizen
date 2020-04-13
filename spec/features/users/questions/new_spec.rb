fb37c5879174646b08807134f806275bf75eb092
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-12 21:05:28 -0600

    Create questions test

--- spec/features/users/questions/new_spec.rb
+++ spec/features/users/questions/new_spec.rb
@@ -5,6 +5,21 @@ RSpec.describe 'As a user' do
     before :each do
       @user = create(:user)
       @user.confirm
+
+      visit '/'
+
+      expect(page).to have_content("Sign In")
+
+      click_on "Sign In"
+
+      expect(current_path).to eq('/users/sign_in')
+
+      fill_in :user_email, with: @user.email
+      fill_in :user_password, with: @user.password
+
+      click_on "Log in"
+
+      expect(current_path).to eq("/")
     end
 
     it "I can navigate from welcome page to post a new technical question" do
@@ -15,9 +30,39 @@ RSpec.describe 'As a user' do
       subject = 'Ruby methods'
       content = 'What is reduce good for?'
 
-      within
+      within '.new_question' do
+        choose 'question_forum_0'
+        fill_in :question_subject, with: subject
+        fill_in :question_content, with: content
+        click_on 'Submit Question'
+      end
 
+      expect(current_path).to eq('/technical_forum')
+      expect(page).to have_content(subject)
+      expect(page).to have_content(content)
+      expect(page).to have_content("Your question was successfully submitted!")
+    end
 
+    it "I can navigate from welcome page to post a new professional question" do
+
+      visit '/questions/new'
+
+      expect(page).to have_content("Post a Question:")
+
+      subject = 'Jobs'
+      content = 'How do I get one?'
+
+      within '.new_question' do
+        choose 'question_forum_1'
+        fill_in :question_subject, with: subject
+        fill_in :question_content, with: content
+        click_on 'Submit Question'
+      end
+
+      expect(current_path).to eq('/professional_forum')
+      expect(page).to have_content(subject)
+      expect(page).to have_content(content)
+      expect(page).to have_content("Your question was successfully submitted!")
     end
   end
 end

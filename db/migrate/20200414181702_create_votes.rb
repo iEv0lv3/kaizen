15fca82d44635a61f3b20bc5f2ab0fe101cddeeb
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-14 12:22:06 -0600

    Add database change for polymorphic voting

---
+++ db/migrate/20200414181702_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :votes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :upvote, default: 1
+
+      t.references :voteable, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end

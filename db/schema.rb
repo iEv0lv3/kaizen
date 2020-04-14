807e177749c03d70b2a234bed62dda57dd909ac7
sasloan <sebastiansloan94@gmail.com>
2020-04-14 08:59:16 -0600

    Created seeds to use in development

--- db/schema.rb
+++ db/schema.rb
@@ -10,76 +10,77 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_414_000_618) do
+ActiveRecord::Schema.define(version: 2020_04_14_000618) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'upvotes', default: 1
-    t.integer 'awards'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'user_id', null: false
-    t.bigint 'question_id', null: false
-    t.index ['question_id'], name: 'index_answers_on_question_id'
-    t.index ['user_id'], name: 'index_answers_on_user_id'
+  create_table "answers", force: :cascade do |t|
+    t.text "content"
+    t.integer "upvotes", default: 1
+    t.integer "awards"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.text 'content'
-    t.integer 'upvotes'
-    t.integer 'awards'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'commentable_type'
-    t.bigint 'commentable_id'
-    t.bigint 'user_id', null: false
-    t.index %w[commentable_type commentable_id], name: 'index_comments_on_commentable_type_and_commentable_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "upvotes", default: 1
+    t.integer "awards"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "commentable_type"
+    t.bigint "commentable_id"
+    t.bigint "user_id", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'subject'
-    t.text 'content'
-    t.integer 'upvotes', default: 1
-    t.integer 'awards'
-    t.integer 'forum'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_questions_on_user_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "subject"
+    t.text "content"
+    t.integer "upvotes", default: 1
+    t.integer "awards"
+    t.integer "forum"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.integer 'status'
-    t.string 'cohort'
-    t.string 'gh_token'
-    t.string 'so_token'
-    t.text 'bio'
-    t.string 'avatar', default: 'https://i.imgur.com/bdnuBf1.jpg'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'gh_login'
-    t.string 'so_uid'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "status"
+    t.string "cohort"
+    t.string "gh_token"
+    t.string "so_token"
+    t.text "bio"
+    t.string "avatar", default: "https://i.imgur.com/bdnuBf1.jpg"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "gh_login"
+    t.string "so_uid"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'answers', 'users'
-  add_foreign_key 'comments', 'users'
-  add_foreign_key 'questions', 'users'
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "comments", "users"
+  add_foreign_key "questions", "users"
 end

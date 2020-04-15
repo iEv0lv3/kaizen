ba7897653f00f3873d733d7cdabb07c1344e35b8
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-14 18:23:33 -0600

    Fix comment edit form to be a form_with

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,6 @@ Rails.application.routes.draw do
     post '/questions', to: 'users/questions#create'
     get '/questions/:id/edit', to: 'users/questions#edit', as: :edit_question
     patch '/questions/:id/edit', to: 'users/questions#update'
-    patch '/questions/:id', to: 'users/questions#upvote', as: 'question_upvote'
-    patch '/questions/:id/answers/:id', to: 'users/answers#upvote', as: 'answer_upvote'
-    patch '/questions/:id/comments/:id', to: 'users/question_comments#upvote', as: 'question_comment_upvote'
-    patch '/questions/:id/answers/:id/comments/:id', to: 'users/answer_comments#upvote', as: 'answer_comment_upvote'
     delete '/questions/:id', to: 'users/questions#destroy'
 
     scope module: :users do
@@ -25,11 +21,13 @@ Rails.application.routes.draw do
         get '/:question_id/answers/:answer_id/edit', to: 'answers#edit', as: 'edit_answer'
         patch '/:question_id/answers/:answer_id', to: 'answers#update', as: 'update_answer'
         delete '/:question_id/answers/:answer_id/delete', to: 'answers#destroy', as: 'delete_answer'
+
         get '/:question_id/comments/new', to: 'question_comments#new', as: 'new_question_comment'
         post '/:question_id/comments', to: 'question_comments#create', as: 'create_new_question_comment'
         get '/:question_id/comments/:comment_id/edit', to: 'question_comments#edit', as: 'edit_question_comment'
         patch '/:question_id/comments/:comment_id', to: 'question_comments#update', as: 'update_question_comment'
         delete '/:question_id/comments/:comment_id/delete', to: 'question_comments#destroy', as: 'delete_question_comment'
+
         get '/:question_id/answers/:answer_id/comments/new', to: 'answer_comments#new', as: 'new_answer_comment'
         post '/:question_id/answers/:answer_id/comments', to: 'answer_comments#create', as: 'create_new_answer_comment'
         get '/:question_id/answers/:answer_id/comments/:comment_id/edit', to: 'answer_comments#edit', as: 'edit_answer_comment'
@@ -37,6 +35,11 @@ Rails.application.routes.draw do
         delete '/:question_id/answers/:answer_id/comments/:comment_id/delete', to: 'answer_comments#destroy', as: 'delete_answer_comment'
       end
     end
+
+    post '/questions/:id', to: 'users/upvotes#question_upvotes', as: 'question_upvote'
+    post '/questions/:id/answers/:id', to: 'users/upvotes#answer_upvotes', as: 'answer_upvote'
+    post '/questions/:id/comments/:id', to: 'users/upvotes#question_comment_upvotes', as: 'question_comment_upvote'
+    post '/questions/:id/answers/:id/comments/:id', to: 'users/upvotes#answer_comment_upvotes', as: 'answer_comment_upvote'
   end
 
   root to: 'welcome#index'

49bab0cd46b880293def896c22c0d0611888614c
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-14 20:24:32 -0600

    Added model method tests for incrementing upvotes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -11,4 +11,40 @@ RSpec.describe Answer, type: :model do
     it { should have_many :comments }
     it { should have_many :votes }
   end
+
+  describe '#increment_upvotes' do
+    before :each do
+      user_1 = User.create!(
+        email: 'user_1@turing.io',
+        password: 'user_1',
+        first_name: 'User',
+        last_name: '1',
+        cohort: '1811',
+        status: 1
+      )
+
+      question = Question.create!(
+        subject: 'Ruby methods',
+        content: 'What is attr_reader?',
+        upvotes: 1,
+        forum: 0,
+        user_id: user_1.id
+      )
+
+      @answer = Answer.create!(
+        content: 'An answer to a question',
+        upvotes: 1,
+        question_id: question.id,
+        user_id: user_1.id
+      )
+    end
+
+    it 'can increment upvotes' do
+      expect(@answer.upvotes).to eq(1)
+
+      @answer.increment_upvotes
+
+      expect(@answer.upvotes).to eq(2)
+    end
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,46 @@ RSpec.describe Comment, type: :model do
     it { should belong_to :user }
     it { should have_many :votes }
   end
+
+  describe '#increment_upvotes' do
+    before :each do
+      user_1 = User.create!(
+        email: 'user_1@turing.io',
+        password: 'user_1',
+        first_name: 'User',
+        last_name: '1',
+        cohort: '1811',
+        status: 1
+      )
+
+      question = Question.create!(
+        subject: 'Ruby methods',
+        content: 'What is attr_reader?',
+        upvotes: 1,
+        forum: 0,
+        user_id: user_1.id
+      )
+
+      answer = Answer.create!(
+        content: 'An answer to a question',
+        upvotes: 1,
+        question_id: question.id,
+        user_id: user_1.id
+      )
+
+      @comment = answer.comments.create!(
+        content: 'This is a comment',
+        upvotes: 1,
+        user_id: user_1.id
+      )
+    end
+
+    it 'can increment upvotes' do
+      expect(@comment.upvotes).to eq(1)
+
+      @comment.increment_upvotes
+
+      expect(@comment.upvotes).to eq(2)
+    end
+  end
 end

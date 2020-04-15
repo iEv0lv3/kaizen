b8d9dd9fdb0325c524a5afd92be02fc4cab429bb
sasloan <sebastiansloan94@gmail.com>
2020-04-15 12:02:35 -0600

    modified user_name in faker and other tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
+    user_name { Faker::Internet.username}
     password { Faker::Internet.password }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }

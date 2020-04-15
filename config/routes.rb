289f7e589fe98c6ae4ebd73002f6cdf708351e56
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-14 20:21:39 -0600

    Revoce unneccessary route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/profile', to: 'users/profile#show'
     get '/profile/edit', to: 'users/profile#edit'
-    get '/profile/edit_password', to: 'users/profile#edit_password'
     patch '/profile', to: 'users/profile#update'
     match '/users/:id' => 'users/profile#destroy', :via => :delete, :as => :destroy_user
 

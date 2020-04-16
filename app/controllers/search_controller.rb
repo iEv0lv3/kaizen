ea3df6f8d222e96c2159d842df588fc2417a8692
David Holtkamp <david.hholtkamp@gmail.com>
2020-04-16 12:01:20 -0600

    Utilize search params private method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 class SearchController < ApplicationController
   def index
-    search_query = Question.search(params[:search]) + Answer.search(params[:search])
+    search_query = Question.search(search_params) + Answer.search(search_params)
     @results = SearchFacade.new(search_query)
-
     if params[:search].empty?
       flash[:warning] = "You must enter something to search."
       redirect_back(fallback_location: root_path)

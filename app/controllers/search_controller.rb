7e6208411f5f86aa58e433a993ef1543323ec88b
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-16 13:43:16 -0600

    Fixed search params and search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,13 @@
 class SearchController < ApplicationController
   def index
-    search_query = Question.search(search_params) + Answer.search(search_params)
+    search_query = Question.search(params[:search]) + Answer.search(params[:search])
     @results = SearchFacade.new(search_query)
+
     if params[:search].empty?
       flash[:warning] = "You must enter something to search."
       redirect_back(fallback_location: root_path)
     elsif @results.sorted.empty?
       flash[:warning] = 'Improve your query for better results.'
     end
-  end
-
-  private
-
-  def search_params
-    params.permit(:search)
   end
 end

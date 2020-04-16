6894c2b2d45d888a54f7814b9344c7440a4572c6
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-16 12:00:46 -0600

    Move test to own file

---
+++ spec/features/questions/answers/verify_answers_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  before :each do
+    @user_1 = create(:user)
+    @user_2 = create(:user)
+    @user_3 = create(:user)
+
+    @user_1.confirm
+
+    visit root_path
+
+    click_on 'Sign In'
+
+    fill_in :user_email, with: @user_1.email
+    fill_in :user_password, with: @user_1.password
+
+    click_on 'Log in'
+
+    @question = Question.create!(
+      subject: 'User Question',
+      content: 'This is a question written by a user',
+      forum: 0,
+      upvotes: 3,
+      user_id: @user_1.id
+    )
+
+    @answer_1 = @question.answers.create!(
+      content: 'This is the best answer to user_1s question',
+      user_id: @user_2.id,
+      upvotes: 30,
+      verification: 0
+    )
+
+    @answer_2 = @question.answers.create!(
+      content: 'This is the second best answer to user_1s question',
+      user_id: @user_3.id,
+      upvotes: 10,
+      verification: 0
+    )
+  end
+
+  it 'can verify an answer on a question' do
+    visit question_show_path(@question)
+
+    within "#answer-#{@answer_1.id}" do
+      click_on 'Verify Answer'
+    end
+
+    expect(page).to have_content(':: Verified Answer ::')
+  end
+end

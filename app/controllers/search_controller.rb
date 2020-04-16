9a77b74de38ef8c008efce515c7ab6a1e19c330a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-16 14:57:49 -0600

    Add rubocop fixes for style

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
     @results = SearchFacade.new(search_query)
 
     if params[:search].empty?
-      flash[:warning] = "You must enter something to search."
+      flash[:warning] = 'You must enter something to search.'
       redirect_back(fallback_location: root_path)
     elsif @results.sorted.empty?
       flash[:warning] = 'Improve your query for better results.'

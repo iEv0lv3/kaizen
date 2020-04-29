4341b07db10ef15ff5e9ef1a5bcecc5abc62d47e
Jordan <jordan.wallace.williams@gmail.com>
2020-04-28 23:38:59 -0600

    Update aws_es service

--- app/services/aws_es_service.rb
+++ app/services/aws_es_service.rb
@@ -1,13 +1,18 @@
 require 'elasticsearch'
+require 'elasticsearch/api'
+require 'elasticsearch/rails'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday_middleware/aws_sigv4'
 
 class AwsEsService
-  attr_reader :elasticsearch
+  include Elasticsearch::API
+
+  attr_reader :elasticsearch, :es2
 
   def initialize
     @elasticsearch = conn
+    @es2 = conn2
   end
 
   def api_get(endpoint)
@@ -15,16 +20,38 @@ class AwsEsService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def search(query)
+    response = @elasticsearch.get "/_search?q=#{query}"
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def api_search(query)
+    response = @elasticsearch.post '/_search' do |req|
+      req.headers[:content_type] = 'application/json'
+      req.body = AwsSearch.new(query).json_build
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   private
 
   def conn
-    Faraday.new(url: 'https://search-kaizen-bnz2vfctyqunfmcvryeiw7pfx4.us-west-2.es.amazonaws.com') do |faraday|
+    Elasticsearch::Model.client = Faraday.new(url: ENV['AWS_ES']) do |faraday|
       faraday.request :aws_sigv4,
                       service: 'es',
                       region: 'us-west-2',
                       access_key_id: ENV['AWS_ACCESS_KEY'],
                       secret_access_key: ENV['AWS_SECRET_KEY']
+    end
+  end
 
+  def conn2
+    Elasticsearch::Client.new(url: ENV['AWS_ES']) do |f|
+      f.request :aws_sigv4,
+                service: 'es',
+                region: 'us-west-2',
+                access_key_id: ENV['AWS_ACCESS_KEY'],
+                secret_access_key: ENV['AWS_SECRET_KEY']
     end
   end
 end

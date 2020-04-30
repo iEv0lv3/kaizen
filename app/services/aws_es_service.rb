144800ec8dc7cdf82bab99297568db38693d4716
Jordan <jordan.wallace.williams@gmail.com>
2020-04-30 12:48:59 -0600

    Cleanup service: remove comments and unnecessary connection

--- app/services/aws_es_service.rb
+++ app/services/aws_es_service.rb
@@ -12,7 +12,6 @@ class AwsEsService
 
   def initialize
     @elasticsearch = conn
-    @es2 = conn2
   end
 
   def api_get(endpoint)
@@ -26,9 +25,7 @@ class AwsEsService
   end
 
   def api_search(query)
-    # Below could be the index for the sample data
-    # /kibana_sample_data_flights/_search
-    response = @elasticsearch.post '/kibana_sample_data_flights/_search' do |req|
+    response = @elasticsearch.post '/_search' do |req|
       req.headers[:content_type] = 'application/json'
       req.body = AwsSearch.new(query).json_build
     end
@@ -46,44 +43,4 @@ class AwsEsService
                       secret_access_key: ENV['AWS_SECRET_KEY']
     end
   end
-
-  def conn2
-    Elasticsearch::Client.new(url: ENV['AWS_ES']) do |f|
-      f.request :aws_sigv4,
-                service: 'es',
-                region: 'us-west-2',
-                access_key_id: ENV['AWS_ACCESS_KEY'],
-                secret_access_key: ENV['AWS_SECRET_KEY']
-    end
-  end
 end
-
-  #  :mappings=>
-  #   {:properties=>
-  #     {:AvgTicketPrice=>{:type=>"float"},
-  #      :Cancelled=>{:type=>"boolean"},
-  #      :Carrier=>{:type=>"keyword"},
-  #      :Dest=>{:type=>"keyword"},
-  #      :DestAirportID=>{:type=>"keyword"},
-  #      :DestCityName=>{:type=>"keyword"},
-  #      :DestCountry=>{:type=>"keyword"},
-  #      :DestLocation=>{:type=>"geo_point"},
-  #      :DestRegion=>{:type=>"keyword"},
-  #      :DestWeather=>{:type=>"keyword"},
-  #      :DistanceKilometers=>{:type=>"float"},
-  #      :DistanceMiles=>{:type=>"float"},
-  #      :FlightDelay=>{:type=>"boolean"},
-  #      :FlightDelayMin=>{:type=>"integer"},
-  #      :FlightDelayType=>{:type=>"keyword"},
-  #      :FlightNum=>{:type=>"keyword"},
-  #      :FlightTimeHour=>{:type=>"keyword"},
-  #      :FlightTimeMin=>{:type=>"float"},
-  #      :Origin=>{:type=>"keyword"},
-  #      :OriginAirportID=>{:type=>"keyword"},
-  #      :OriginCityName=>{:type=>"keyword"},
-  #      :OriginCountry=>{:type=>"keyword"},
-  #      :OriginLocation=>{:type=>"geo_point"},
-  #      :OriginRegion=>{:type=>"keyword"},
-  #      :OriginWeather=>{:type=>"keyword"},
-  #      :dayOfWeek=>{:type=>"integer"},
-  #      :timestamp=>{:type=>"date"}}},

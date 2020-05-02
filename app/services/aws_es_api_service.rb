1f3e858a3ee460210818fffd6d2fdc8b10e837a7
Jordan <jordan.wallace.williams@gmail.com>
2020-05-01 18:24:46 -0600

    Add AWS ES API service that connects using Elasticsearch::Client

---
+++ app/services/aws_es_api_service.rb
@@ -0,0 +1,29 @@
+require 'elasticsearch'
+require 'elasticsearch/model'
+require 'elasticsearch/api'
+require 'elasticsearch/rails'
+require 'faraday'
+require 'faraday_middleware'
+require 'faraday_middleware/aws_sigv4'
+
+class AwsEsService
+  include Elasticsearch::API
+
+  attr_reader :elasticsearch
+
+  def initialize
+    @elasticsearch = conn
+  end
+
+  private
+
+  def conn
+    Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['AWS_ES'], port: 443) do |faraday|
+      faraday.request :aws_sigv4,
+                      service: 'es',
+                      region: 'us-west-2',
+                      access_key_id: ENV['AWS_ACCESS_KEY'],
+                      secret_access_key: ENV['AWS_SECRET_KEY']
+    end
+  end
+end

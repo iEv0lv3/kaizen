8a674649a2a1ba9986446856ede1e0c70ba98ec7
Jordan <jordan.wallace.williams@gmail.com>
2020-05-02 11:39:13 -0600

    Comment out logger in application.rb file to clean up test log output

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TuringSo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.logger = Logger.new(STDOUT)
+    # config.logger = Logger.new(STDOUT)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

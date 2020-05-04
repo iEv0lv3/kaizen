eaae9fb6a58ced97ce0ea67e2247b7430247e254
Jordan <jordan.wallace.williams@gmail.com>
2020-05-04 00:46:17 -0600

    Update possible initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,15 +1,18 @@
-# if Rails.env == 'production'
-#   require 'elasticsearch'
-#   require 'elasticsearch/api'
-#   require 'faraday'
-#   require 'faraday_middleware'
-#   require 'faraday_middleware/aws_sigv4'
+# require 'elasticsearch'
+# require 'elasticsearch/api'
+# require 'elasticsearch/model'
+# require 'elasticsearch/transport'
+# require 'elasticsearch/rails'
+# require 'faraday'
+# require 'faraday_middleware'
+# require 'faraday_middleware/aws_sigv4'
 
-#   Elasticsearch::Model.client = Elasticsearch::Client.new(host: ENV['AWS_ES'], port: 443) do |faraday|
-#     faraday.request :aws_sigv4,
-#                     service: 'es',
-#                     region: 'us-west-2',
-#                     access_key_id: ENV['AWS_ACCESS_KEY'],
-#                     secret_access_key: ENV['AWS_SECRET_KEY']
-#   end
+# client = Elasticsearch::Client.new(host: ENV['AWS_ES'], port: 80) do |faraday|
+#   faraday.request :aws_sigv4,
+#                   service: 'es',
+#                   region: 'us-west-2',
+#                   access_key_id: ENV['AWS_ACCESS_KEY'],
+#                   secret_access_key: ENV['AWS_SECRET_KEY']
 # end
+
+# Elasticsearch::Model.client = client
